ebb4f5a6826919c5e8a354e9216aaebfe4343c1b
Jens Ungerer <github@jens-ungerer.de>
2019-07-27 13:39:57 +0200

    start.bat using logic and between single steps of batch file

--- start.bat
+++ start.bat
@@ -1,7 +1,1 @@
-CALL cd client
-CALL npm run build
-CALL cd ..
-CALL cd server
-CALL npm run build
-CALL cd ..
-CALL node .\server\dist\index.js
+CALL cd client && CALL npm run build && CALL cd .. && CALL cd server && CALL npm run build && CALL cd .. && CALL node .\server\dist\index.js
