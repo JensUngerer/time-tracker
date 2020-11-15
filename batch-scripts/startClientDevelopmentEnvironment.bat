e0cab74801b98297b4d6f003ffe79b7a846bea44
Jens Ungerer <github@jens-ungerer.de>
2020-11-15 14:34:00 +0100

    extending build script

--- batch-scripts/startClientDevelopmentEnvironment.bat
+++ batch-scripts/startClientDevelopmentEnvironment.bat
@@ -1,2 +1,2 @@
 REM https://stackoverflow.com/questions/9392874/bat-file-open-new-cmd-window-and-execute-a-command-in-there
-code .\..\client && start cmd /k .\serveClient.bat
+code .\..\client && start cmd /k .\buildClient && start cmd /k .\serveClient.bat
